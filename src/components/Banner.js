e19729652872237723ab2d4019a137f2f4318c9a
Uwem Precious <155549089+PelvinDreams@users.noreply.github.com>
2025-01-08 14:00:57 +0100

    working on services

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -18,19 +18,34 @@ import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 
 // variant
-import { fadeIn } from "../variants"; 
+import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
+    >
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center gap-y-8 lg:items-center lg:gap-x-16">
           {/* text section */}
           <div className="flex-1 text-center lg:text-left">
-            <h1 className="text-4xl lg:text-[110px] font-bold leading-[0.8] text-[55px]">
-              Precious <span className="text-accent">Uwem</span>
-            </h1>
-            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-4xl lg:text-[110px] font-bold leading-[0.8] text-[55px]"
+            >
+              Aiden <span className="text-accent">Ben</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+            >
               <span className="text-white mr-4">I am a</span>
               <TypeAnimation
                 sequence={[
@@ -48,20 +63,38 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               />
-            </div>
-            <p className="mb-8 max-w-lg max-auto lg:max-0">
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg max-auto lg:max-0"
+            >
               Lorem Ipsum is dummy text widely used in the printing and
               typesetting industry since the 1500s.
-            </p>
-            <div className="flex gap-4 mb-8 max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex gap-4 mb-8 max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
               <button className="btn btn-lg">Contact me</button>
               <a href="#" className="text-gradient btn-link">
                 My Portfolio
               </a>
-            </div>
+            </motion.div>
 
             {/* socials */}
-            <div className="flex justify-center lg:justify-start text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex justify-center lg:justify-start text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
               <a href="#" className="hover:text-accent">
                 <FaYoutube />
               </a>
@@ -77,11 +110,16 @@ const Banner = () => {
               <a href="#" className="hover:text-accent">
                 <FaTwitter />
               </a>
-            </div>
+            </motion.div>
           </div>
 
           {/* image section */}
-          <div className="flex-1">
+          <motion.div
+            variants={fadeIn("up", 0.8)}
+            initial="hidden"
+            whileInView={"show"}
+            className=" hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]"
+          >
             <motion.img
               src={Image}
               alt="Avatar of Ben Aiden"
@@ -89,7 +127,7 @@ const Banner = () => {
               initial="hidden"
               animate="show"
             />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,93 @@
 import React from "react";
+// icon
+import { BsArrowUpRight } from "react-icons/bs";
+
+// motion
+import { motion } from "framer-motion";
+
+// Variant
+import { fadeIn } from "../variants";
+
+// Services
+
+const services = [
+  {
+    name: "UI/UX Designer",
+
+    description:
+      "Lorem Ipsum is dummy text widely used in the printing and typesetting industry since the 1500s.",
+
+    link: "Learn more",
+  },
+  {
+    name: "Development",
+
+    description:
+      "Lorem Ipsum is dummy text widely used in the printing and typesetting industry since the 1500s.",
+
+    link: "Learn more",
+  },
+  {
+    name: "YouTuber",
+
+    description:
+      "Lorem Ipsum is dummy text widely used in the printing and typesetting industry since the 1500s.",
+
+    link: "Learn more",
+  },
+  {
+    name: "Digital Marketer",
+
+    description:
+      "Lorem Ipsum is dummy text widely used in the printing and typesetting industry since the 1500s.",
+
+    link: "Learn more",
+  },
+];
 
 const Services = () => {
   return (
-    <div className="section" id="services">
-      Services
-    </div>
+    <section className="section" id="services">
+      <div className="conatiner mx-auto">
+        <div className="flex flex-col lg:flex-row ">
+          {/* text and image */}
+          <div className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
+            <h2 className="h2 text-accent mb-6">What I Deliver</h2>
+            <h3 className="h3 max-w-[455px] mb-16">
+              Lorem Ipsum is dummy text widely used in the printing and
+              typesetting industry since the 1500s.
+            </h3>
+
+            <button className="btn btn-sm">See my works</button>
+          </div>
+          {/* services */}
+          <div className="flex-1">
+            {/* services list */}
+            <div>
+              {services.map((services, index) => {
+                {
+                  /* destruscture service */
+                }
+
+                const { name, description, link } = services;
+
+                return (
+                  <div className="border-b border-white/20 h-[146px] mb-[38px] flex" key={index}>
+                    <div className="max-w-[476px]">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {name}
+                      </h4>
+                      <p className="font-secondary leading-tight">{description}</p>
+                    </div>
+                    <div>links</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
